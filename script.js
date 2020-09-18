d97d893ed92dca01d5cd085346a8069e6882dc51
Kato <kubkate@gmail.com>
2020-09-17 21:00:19 -0700

    in html ser form attribute to novalidate and setup fucntion in script

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+  // using Constraint API
+  isValid = form.checkValidity();
+  console.log(isValid);
+}
+
+// Validate the form and store data
+function processFormData(e) {
+  e.preventDefault();
+  validateForm();
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData);
